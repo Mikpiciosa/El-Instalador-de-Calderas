db99f6002d83ddf93967bea55c507bd445354f52
Micaela Muñoz <132718487+Mikpiciosa@users.noreply.github.com>
2024-02-03 02:04:04 -0300

    add galery and footer

--- src/components/FrontPage/FrontPage.jsx
+++ src/components/FrontPage/FrontPage.jsx
@@ -2,28 +2,26 @@ import "./FrontPage.css";
 
 export const FrontPage = () => {
   return (
-    <main>
-      <section className="frontpage">
-        <section className="frontpage__text">
-          <p className="front__text">
-            Buscamos brindar a nuestros clientes la climatización más
-            confortable con excelencia en instalación y equipamiento.
-          </p>
-          <h2 className="front__title">El Instalador de Calderas</h2>
-          <button className="front__btn">Conocé más</button>
-        </section>
-        <aside className="frontpage__rrss">
-          <a href="">
-            <img src="./WhatsApp.svg" alt="icono para ir a whatsapp" />
-          </a>
-          <a href="">
-            <img src="./Facebook.svg" alt="icono para ir a facebook" />
-          </a>
-          <a href="">
-            <img src="./Instagram.svg" alt="icono para ir a instagram" />
-          </a>
-        </aside>
+    <section className="frontpage">
+      <section className="frontpage__text">
+        <p className="front__text">
+          Buscamos brindar a nuestros clientes la climatización más confortable
+          con excelencia en instalación y equipamiento.
+        </p>
+        <h2 className="front__title">El Instalador de Calderas</h2>
+        <button className="front__btn">Conocé más</button>
       </section>
-    </main>
+      <aside className="frontpage__rrss">
+        <a href="">
+          <img src="./WhatsApp.svg" alt="icono para ir a whatsapp" />
+        </a>
+        <a href="">
+          <img src="./Facebook.svg" alt="icono para ir a facebook" />
+        </a>
+        <a href="">
+          <img src="./Instagram.svg" alt="icono para ir a instagram" />
+        </a>
+      </aside>
+    </section>
   );
 };
---
+++ src/components/Galery/Galery.jsx
@@ -0,0 +1,10 @@
+import "./Galery.css";
+
+export const Galery = () => {
+  return (
+    <section className="galery">
+      <h2 className="galery__title">Galeria</h2>
+      <p className="galery__text">Conoce un poco de nuestro trabajo</p>
+    </section>
+  );
+};
