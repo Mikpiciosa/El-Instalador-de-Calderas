f38a592be7edbcdb2627eab4923dbfe7d9d3b552
Micaela Muñoz <132718487+Mikpiciosa@users.noreply.github.com>
2024-02-05 19:10:32 -0300

    changes

--- src/components/FrontPage/FrontPage.jsx
+++ src/components/FrontPage/FrontPage.jsx
@@ -4,15 +4,17 @@ import "./FrontPage.css";
 export const FrontPage = () => {
   return (
     <section className="frontpage">
-      <section className="frontpage__text">
-        <p className="front__text">
-          Buscamos brindar a nuestros clientes la climatización más confortable
-          con excelencia en instalación y equipamiento.
-        </p>
-        <h2 className="front__title">El Instalador de Calderas</h2>
-        <button className="front__btn">Conocé más</button>
+      <section className="cnt">
+        <section className="frontpage__text">
+          <p className="front__text">
+            Buscamos brindar a nuestros clientes la climatización más
+            confortable con excelencia en instalación y equipamiento.
+          </p>
+          <h2 className="front__title">El Instalador de Calderas</h2>
+          <button className="front__btn">Conocé más</button>
+        </section>
+        <AsideFooter alternative={true} none={false}></AsideFooter>
       </section>
-      <AsideFooter alternative={true} none={false}></AsideFooter>
     </section>
   );
 };
